e0c6bc82e394c6d48c02c0ae13eedde7abb20ee0
LucianAlexandru <alexandru_f_lucian@yahoo.com>
2016-12-19 12:38:07 +0200

    AutoCorrect class
    Purpose: suggesting a correction for the input

---
+++ TextProcessing/api/nltk/autocorrect.py
@@ -0,0 +1,33 @@
+import enchant
+
+class AutoCorrect:
+
+    wordDict = None
+
+    @staticmethod
+    def initDict():
+        if AutoCorrect.wordDict is None:
+            AutoCorrect.wordDict = enchant.Dict('en_US')
+    
+    @staticmethod
+    def suggest (sentence):
+        AutoCorrect.initDict()
+        words = sentence.split()
+        suggestions = []
+        foundError = False
+        for word in words:
+            if AutoCorrect.wordDict.check (word) == False:
+                foundError = True
+                try:
+                    suggestions.append (AutoCorrect.wordDict.suggest(word)[0])
+                except IndexError:
+                    raise SuggestionNotFoundException
+            else:
+                suggestions.append (word)
+        if foundError == False:
+            return None
+        else:
+            return ' '.join(suggestions)
+            
+class SuggestionNotFoundException (Exception):
+    pass
